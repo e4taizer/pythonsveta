ce45f56bdabd0d29098eeaab6b612a74f79bb88a
e4taizer <pamal@ya.ru>
2024-03-24 19:49:40 +0300

    proba

--- urok2.py
+++ urok2.py
@@ -1,9 +1,27 @@
-print(42)
-print(type(42))
-print("42")
-print(type("42"))
+# print(42)
+# print(type(42))
+# print("42")
+# print(type("42"))
 #print(1.5)
 #print(type(1.5))
 #
 #print(2.4j)
 #print(type(2.4j))
+# first_number = 5
+# print("тип числа second_number является", type(first_number))
+# second_number = 3.11j
+# print( type(second_number))
+# testone = True
+# print("тип данных:",type(testone))
+# print("""fdfdfdfdfdfddfdfdfdfdfdfdf
+# fdfddfdfdffdfdfdfdfdffdfdfdfdfdfdfdfdfdfdfdfd
+# """)
+# print(type("""fdfdfdfdfdfddfdfdfdfdfdfdf
+# fdfddfdfdffdfdfdfdfdffdfdfdfdfdfdfdfdfdfdfdfd
+# """))
+# list1 = [1,"hi", "python",True]
+# print(type(list1))
+# print([1,2, True,"True"])
+#print({1: "Kitty cat", "stop": 16, 3:False })
+#new_set = {}
+print({1,2,1})
