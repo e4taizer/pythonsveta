303b9ddb64da10b29b218201ee00310bb2a0e786
e4taizer <pamal@ya.ru>
2024-03-27 20:43:22 +0300

    do 45 min yroka

---
+++ urok3.py
@@ -0,0 +1,27 @@
+# a= 123
+# print(a)
+# b = a
+# print(b)
+# print(id(a), id(b))
+# #тоже самое только  b=123
+# a= 123
+# print(a)
+# b = 123
+# print(b)
+# print(id(a), id(b))
+# a= "Hello world"
+# print(a)
+# b = "Hello world"
+# print(b)
+# print(id(a), id(b))
+# a="he"
+# b="llo"
+# word = a+b
+# print(word)
+# print(id(a))
+# print(id(b))
+# print(id(word))
+# ckoroct1 = 1000000
+# word = ckoroct1 +100
+# print(word)
+
