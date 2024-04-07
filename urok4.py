255220c75b6fb022d021291e4eb9102e7338271c
e4taizer <pamal@ya.ru>
2024-04-07 21:10:51 +0300

    w

---
+++ urok4.py
@@ -0,0 +1,18 @@
+# x = 5
+# x= -x
+# print(x)
+# x= +x
+# print(x)
+# x= 5
+# y=10
+# z=2.2
+# print(x//z)
+# y= 12
+# print(y%3)
+# list_1 = [1,2]
+# list_2 = [3,4]
+# print(list_1 +list_2)
+x=3
+y=4
+result =(x**2 + y**2) **0.5
+print(result)
