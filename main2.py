35a491b0f70601d834938843f711272c5a0d9fe6
e4taizer <pamal@ya.ru>
2024-04-07 21:11:14 +0300

    w

--- main2.py
+++ main2.py
@@ -3,8 +3,16 @@
 # third = 30000.11111111
 # print("На заводы ямальского округа",first,"были доставлены сепараторы модели",second, ","
 # "которые могут работать с большим объемом материала, при условии не превышения лимита скорости в",third)
-
-
+# x= "Hello"
+# print(x)
+# y= "1234"
+# print(int(y))
+# print(type(y))
+# print((x))
+# string = "hello"
+# string = bool(string)
+# string = int(string)
+# print(type(string))
 
 
 
