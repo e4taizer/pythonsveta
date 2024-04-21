5215b870e00c6136244c1651c9ee55e25af7f621
e4taizer <pamal@ya.ru>
2024-04-21 18:17:19 +0300

    w

---
+++ trenajer.py
@@ -0,0 +1,32 @@
+# a = int(input())
+# b = int(input())
+#
+# print(a/b)
+# n = int(input())
+# chas =(n//60)
+# minuty = (n%60)
+# print (chas)
+# print(minuty)
+# n = int(input())
+# print(type(n))
+# n= int(input())
+# print("The next number for the number",n,"is"(n+1),".")
+# print(n)
+# n= int(input())
+# print ("тест проверки "+ str(n))
+# print("тест проверки ", str(n))
+# class1 = int(input())
+# class2 = int(input())
+# class3 = int(input())
+# resultat1 =((class1//2)+(class1%2))
+# resultat2 = ((class2//2)+(class2%2))
+# resultat3 = ((class3//2)+(class3%2))
+# print(resultat1+resultat2+resultat3)
+# r=8**3
+# print(r)
+# print (not "")
+# v = True
+# a = True
+#
+# if v == a and v
+#     print("Текст, который будет выведен")
