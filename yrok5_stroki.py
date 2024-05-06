990cb2ae32cffabdd01ebed4afb8fe83294697b0
e4taizer <pamal@ya.ru>
2024-05-06 20:51:23 +0300

    w

---
+++ yrok5_stroki.py
@@ -0,0 +1,32 @@
+# x= "bug's"
+# str1= "\a"
+# print(str1)
+# str = "строка"
+# str2 = "одна такая"
+# print (str,str2, sep="@")
+# name ="vasia"
+# str= "{2},{1} {0}.{3}".format(5,"manual",10,"test")
+# print (str)
+# str1 ="{:@>10}" .format("выравнивание")
+# print(str1)
+# name = "Anton"
+# str1= "{3} {0},{1} {2}".format(6,10,6,0)
+# print(str1)
+# num1 = 4
+# num2 = 5
+#
+# result = num1<num2
+# str1 = f"{result=}"
+# print (str1)
+# print(type(str1))
+#############################
+# name = "Anton"
+# print(name[-5])
+# str1 = "A1-B2-C3-D4-E5-"
+# print(str1[::2])
+#############################
+# str1 = "ferkekfkleflsl;14948388!!$$^"
+# # print(str1.find("kek"))
+# str1 = "ferkekfkleflsl;14948388!!$$^"
+# print(str1.split("e"))
+#################
