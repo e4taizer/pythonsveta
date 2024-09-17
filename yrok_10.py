38b67c676316214097a0cc54b5a18dfde7756a9c
e4taizer <pamal@ya.ru>
2024-09-17 23:35:01 +0300

    урок 10

---
+++ yrok_10.py
@@ -0,0 +1,19 @@
+######################## СЛОВАРИ
+# dictionary = {} # пустой словарь Словари не хешируются. Изменяемые объекты все не хешируются
+# numbers = dict() #  так же пустой словарь только функцией dict
+##############################
+#numbers ={2:"two",4:"four","six":6}
+#names_and_ages = {"Alex":21,"Marc":18,"Maria":22, 4:"fhor"}
+#print(names_and_ages)
+#names_and_ages = dict(Alex=31,Marc=18,Maria=22)
+#print(names_and_ages)
+#names_and_ages = dict({"Alex":31} , Marc=18, Maria=22)
+#print(names_and_ages)
+####################
+#Создание словаря с повторяющимися ключами
+# awesome_dict = {
+#     "first_key":"first",
+#     "second_key":2,
+#     "first_key":1
+# }
+# print(awesome_dict)
