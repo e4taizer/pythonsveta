43589a25e8698a80059bff4130e791b0ed8e9e87
e4taizer <pamal@ya.ru>
2025-01-29 16:33:50 +0300

    f

---
+++ 1_24/lesson_1_24.py
@@ -0,0 +1,14 @@
+#pytest
+def test_5_x_5():
+    assert  5*5 == 25
+
+def test_6_x_6():
+    assert 6*6 == 35
+
+def test_7_x_7():
+    assert 7*7 == 49
+
+if __name__== '__main__':
+    test_5_x_5()
+    test_6_x_6()
+    test_7_x_7()
